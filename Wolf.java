93ddcf519af5b77ef1a6a58568a07333257ac207
Thielence <thielence@appstate.edu>
2025-10-14 18:48:36 -0400

    Implemented Wolf.java

---
+++ Wolf.java
@@ -0,0 +1,32 @@
+public class Wolf extends Animal
+{
+    public Wolf()
+    {
+        setMovementBehavior(new MovementBehavior() 
+        {
+            @Override
+            public String move()
+            {
+                return "Wolves wandering around...";
+            }
+        });
+
+        setDietBehavior(new DietBehavior()
+        {
+            @Override
+            public String eat()
+            {
+                return "Wolves are omnivores...\nEating rabbits";
+            }
+        });
+
+        setSoundBehavior(new SoundBehavior()
+        {
+            @Override
+            public String makeSound()
+            {
+                return "HOWL";
+            }
+        });
+    }
+}    
