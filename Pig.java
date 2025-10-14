a7f50148e78f441108fe178b4327da2216cecd09
Thielence <thielence@appstate.edu>
2025-10-14 18:47:11 -0400

    Implemented Pig.java

---
+++ Pig.java
@@ -0,0 +1,32 @@
+public class Pig extends Animal
+{
+    public Pig()
+    {
+        setMovementBehavior(new MovementBehavior() 
+        {
+            @Override
+            public String move()
+            {
+                return "Pig wandering...";
+            }
+        });
+
+        setDietBehavior(new DietBehavior()
+        {
+            @Override
+            public String eat()
+            {
+                return "Pigs are omnivores...\nEating roots and insects";
+            }
+        });
+
+        setSoundBehavior(new SoundBehavior()
+        {
+            @Override
+            public String makeSound()
+            {
+                return "OINK";
+            }
+        });
+    }
+}    
